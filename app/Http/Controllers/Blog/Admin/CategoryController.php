bcce944d65c3ebaf20158a9041897e3d2600788c
Yuriy Dyachuk <yuriydyachuk91@gmail.com>
2019-10-27 20:34:14 +0200

    Create:  Menu items/  Admin panel categories  Create Route

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\AdminCategoryUpdateRequest;
 use App\Models\Admin\Category;
 use App\Repositories\Admin\CategoryRepository;
 use Illuminate\Http\Request;
@@ -27,39 +28,119 @@ class CategoryController extends AdminBaseController
         return view('blog.admin.category.index', ['menu' => $menu]);
     }
 
+    /** @throws \Exception */
     public function mydel() {
+        $id = $this->categoryRepository->getRequestID();
+        if (!$id) {
+            return back()->withErrors(['msg' => 'Ошибка с ID']);
+        }
+
+        $children = $this->categoryRepository->checkChildren($id);
+        if ($children) {
+            return back()->withErrors(['msg' => 'Удаление невозможно в кат-рии есть вложеные кат-рии!']);
+        }
+
+        $parents = $this->categoryRepository->checkParentsProductsId($id);
+        if ($parents) {
+            return back()->withErrors(['msg' => 'Удаление невозможно в кат-рии есть товары!']);
+        }
+
+        $delete = $this->categoryRepository->deleteCategory($id);
+        if ($delete) {
+            return redirect()
+                ->route('blog.admin.categories.index')
+                ->with(['success' => "Запись id [$id] удалена"]);
+        } else {
+            return back()->withErrors(['msg' => 'Ошибка удаления!']);
+        }
 
     }
 
     public function create()
     {
-        //
+        $item = new Category();
+        $categoryList = $this->categoryRepository->getComboBosCategories();
+
+
+
+        MetaTag::setTags(['title' => 'Создание новой категории']);
+        return view('blog.admin.category.create',
+            ['categories' => Category::with('children')
+                ->where('parent_id', 0)
+                ->get(),
+                'delimiter' => '-',
+                'item' => $item,
+            ]);
+
     }
 
 
-    public function store(Request $request)
+    public function store(AdminCategoryUpdateRequest $request)
     {
-        //
-    }
+        $name = $this->categoryRepository->checkUniqueName($request->title, $request->parent_id);
+        if ($name) {
+            return back()
+                ->withErrors(['msg' => 'Не может быть в одной и той же категории двух одинаковых.
+                    Выберите другую категорию'])
+                ->withInput();
+        }
 
+        $data = $request->input();
+        $item = new Category();
+        $item->fill($data)->save();
+        if ($item) {
+            return redirect()
+                ->route('blog.admin.categories.create', [$item->id])
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+
+    }
 
     public function show($id)
     {
         //
     }
 
-
-    public function edit($id)
+    public function edit(CategoryRepository $categoryRepository, $id)
     {
-        //
+        $item = $this->categoryRepository->getId($id);
+        if (empty($item)) {
+            abort(404);
+        }
+        MetaTag::setTags(['title' => "Редактирование категории № $id"]);
+        return view('blog.admin.category.edit',
+            ['categories' => Category::with('children')
+                ->where('parent_id', 0)
+                ->get(),
+                'delimiter' => '-',
+                'item' => $item,
+            ]);
     }
 
-
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(AdminCategoryUpdateRequest $request, $id)
+    {$item = $this->categoryRepository->getId($id);
+    if (empty($item)) {
+        return back()
+            ->withErrors(['msg' => "Запись - [{$id}] не найдена"])
+            ->withInput();
+    }
+    $data = $request->all();
+    $result = $item->update($data);
+    if ($result) {
+        return redirect()
+            ->route('blog.admin.categories.edit', $item->id)
+            ->with(['success' => 'Успешно сохранено']);
+    } else {
+        return back()
+            ->withErrors(['msg' => 'Ошибка сохранения'])
+            ->withInput();
     }
 
+    }
 
     public function destroy($id)
     {
--- app/Repositories/Admin/CategoryRepository.php
+++ app/Repositories/Admin/CategoryRepository.php
@@ -10,6 +10,7 @@ namespace App\Repositories\Admin;
 
 use App\Models\Admin\Category as Model;
 use App\Repositories\CoreRepository;
+use DB;
 use Menu as CatMenu;
 
 class CategoryRepository extends CoreRepository
@@ -41,6 +42,51 @@ class CategoryRepository extends CoreRepository
            }
         });
         return $mBuilder;
+    }
+
+    public function checkChildren($id) {
+        $children = $this->startConditions()
+            ->where('parent_id', $id)
+            ->count();
+        return $children;
+    }
+
+    public function checkParentsProductsId($id) {
+        $parents = DB::table('products')
+            ->where('category_id', $id)
+            ->count();
+        return $parents;
+    }
+
+    public function deleteCategory($id) {
+        $delete = $this->startConditions()
+            ->find($id)
+            ->forceDelete();
+        return $delete;
+
+    }
+    public function getComboBosCategories() {
+        $columns = implode(',', [
+            'id',
+            'parent_id',
+            'title',
+            'CONCAT (id, ". ", title) AS combo_title',
+            ]);
+        $result = $this->startConditions()
+            ->selectRaw($columns)
+            ->toBase()
+            ->get();
+        return $result;
+
+    }
+
+    public function checkUniqueName($name, $parent_id) {
+        $name = $this->startConditions()
+            ->where('title', '=', $name)
+            ->where('parent_id', '=', $parent_id)
+            ->exists();
+        return $name;
+
     }
 
 }
